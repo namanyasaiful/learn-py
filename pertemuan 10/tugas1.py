6dd1f61c5fdfa9faaab05208c4c8728e95be7281
namanyasaiful <saifullohfattah023@gmail.com>
2024-11-20 06:34:07 +0700

    pertemuan 10

---
+++ pertemuan 10/tugas1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+suhu = np.array([30,31,32,33,34,35,36,37,38,39,40])
+fahrenheit = suhu * 9/5 - 32
+print(suhu)
+print(fahrenheit)
+# saifullloh fattah bintoro_2408256
---
+++ pertemuan 10/tugas2.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+nilai = np.random.randint(0, 100, 30)
+urut = np.sort(nilai)[::-1]
+print(urut)
+print(urut[:5])
+# saifulloh fattah bintoro_2408256
